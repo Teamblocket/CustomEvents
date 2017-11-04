1ec382f19d3a9c245d4cf041c256db6575d3f254
Angel <bubblicious712@gmail.com>
2017-11-04 14:25:46 -0400

    Update Main.php

--- src/CustomEvents/Main.php
+++ src/CustomEvents/Main.php
@@ -2,15 +2,22 @@
 
 namespace CustomEvents;
 
-use pocketmine\plugin\PluginBase;
+use CustomEvents\Events\TestCustomEvent;
 
-class Main extends PluginBase{
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+
+class Main extends PluginBase implements Listener{
   
   public function onEnable(){
     $this->getServer()->getLogger()->notice('CustomEvents Plugin Enabled and Created by Angel');
   }
   
-  public function testCustomEvent(){
+  public function callTestCustomEvent(){
     $this->getServer()->getPluginManager()->callEvent($ev = new TestCustomEvent($this));
+  }
+  
+  public function onTest(TestCustomEvent $event){
+    $event->doTest();
   }
 }
