04ec8f758d9af24d5c42de8965b23824b9d7abea
Angel <bubblicious712@gmail.com>
2017-11-04 14:18:33 -0400

    Update Main.php

--- src/CustomEvents/Main.php
+++ src/CustomEvents/Main.php
@@ -5,4 +5,12 @@ namespace CustomEvents;
 use pocketmine\plugin\PluginBase;
 
 class Main extends PluginBase{
+  
+  public function onEnable(){
+    $this->getServer()->getLogger()->notice('CustomEvents Plugin Enabled and Created by Angel');
+  }
+  
+  public function testCustomEvent(){
+    $this->getServer()->getPluginManager()->callEvent($ev = new TestCustomEvent($this));
+  }
 }
