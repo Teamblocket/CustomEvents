196cdc533eec51da19f0470f9ef387cb942a22cf
Angel <bubblicious712@gmail.com>
2017-11-04 14:23:46 -0400

    Update TestCustomEvent.php

--- src/CustomEvents/Events/TestCustomEvent.php
+++ src/CustomEvents/Events/TestCustomEvent.php
@@ -2,5 +2,17 @@
 
 namespace CustomEvents\Events;
 
+use CustomEvents\Main;
+
 class TestCustomEvent extends CustomEventAPI{
+  
+  private $plugin;
+  
+  public function __construct(Main $plugin){
+    $this->plugin = $plugin;
+  }
+  
+  public function doTest(){
+    $this->plugin->getServer()->getLogger()->notice('TestCustomEvent fired!');
+  }
 }
