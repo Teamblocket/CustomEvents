6bb2bd459a0f2f56a0f185b66aeb2749808be2e4
Angel <24781244+Teamblocket@users.noreply.github.com>
2018-02-19 08:28:09 -0500

    Update TestCustomEvent.php

--- src/CustomEvents/Events/TestCustomEvent.php
+++ src/CustomEvents/Events/TestCustomEvent.php
@@ -4,15 +4,25 @@ namespace CustomEvents\Events;
 
 use CustomEvents\Main;
 
+/**
+ * Class TestCustomEvent
+ * @package CustomEvents\Events
+ */
 class TestCustomEvent extends CustomEventAPI{
-  
-  private $plugin;
-  
-  public function __construct(Main $plugin){
-    $this->plugin = $plugin;
-  }
-  
-  public function doTest(){
-    $this->plugin->getServer()->getLogger()->notice('TestCustomEvent fired!');
-  }
+
+	/** @var null  */
+	public static $handlerList = null;
+
+	/**
+	 * TestCustomEvent constructor.
+	 * @param Main $plugin
+	 */
+	public function __construct(Main $plugin){
+		parent::__construct($plugin);
+		$this->plugin = $plugin;
+	}
+
+	public function doTest() : void{
+		$this->plugin->getServer()->getLogger()->notice('TestCustomEvent fired!');
+	}
 }
