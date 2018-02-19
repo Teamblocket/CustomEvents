bd0721460b2546898d4bc84d19de69a482b56fd3
Angel <24781244+Teamblocket@users.noreply.github.com>
2018-02-19 08:27:05 -0500

    Update Main.php

--- src/CustomEvents/Main.php
+++ src/CustomEvents/Main.php
@@ -3,22 +3,26 @@
 namespace CustomEvents;
 
 use CustomEvents\Events\TestCustomEvent;
-
-use pocketmine\plugin\PluginBase;
 use pocketmine\event\Listener;
+use pocketmine\plugin\PluginBase;
 
+/**
+ * Class Main
+ * @package CustomEvents
+ */
 class Main extends PluginBase implements Listener{
-  
-  public function onEnable(){
-    $this->getServer()->getLogger()->notice('CustomEvents Plugin Enabled and Created by Angel');
-    $this->callCustomTestEvent();
-  }
-  
-  public function callCustomTestEvent(){
-    $this->getServer()->getPluginManager()->callEvent($ev = new TestCustomEvent($this));
-  }
-  
-  public function onTest(TestCustomEvent $event){
-    $event->doTest();
-  }
+
+	public function onEnable (){
+		$this->getServer()->getLogger()->notice('CustomEvents Plugin Enabled and Created by Angel');
+		$this->getServer()->getPluginManager()->registerEvents($this, $this);
+		$this->getServer()->getPluginManager()->callEvent($ev = new TestCustomEvent($this));
+	}
+
+	/**
+	 * @param TestCustomEvent $event
+	 */
+	public function onTest(TestCustomEvent $event){
+		$event->doTest();
+		$this->getServer()->getLogger()->notice('It works!');
+	}
 }
